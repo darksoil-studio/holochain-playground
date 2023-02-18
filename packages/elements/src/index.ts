7e824f0a89a9449dc4fb49fbc05f326045a28492
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-19 00:44:33 +0100

    Fixed elements

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -5,14 +5,14 @@ export * from './base/base-playground-context.js';
 export * from './base/playground-element.js';
 export * from './base/selectors.js';
 
-export * from './elements/call-zome-fns.js';
-export * from './elements/run-steps.js';
-export * from './elements/dht-cells.js';
-export * from './elements/select-active-dna.js';
-export * from './elements/conductor-admin.js';
-export * from './elements/entry-contents.js';
-export * from './elements/source-chain.js';
-export * from './elements/dht-entries.js';
+export * from './elements/call-zome-fns/index.js';
+export * from './elements/run-steps/index.js';
+export * from './elements/dht-cells/index.js';
+export * from './elements/select-active-dna/index.js';
+export * from './elements/conductor-admin/index.js';
+export * from './elements/entry-contents/index.js';
+export * from './elements/source-chain/index.js';
+export * from './elements/dht-entries/index.js';
 
 export * from './elements/helpers/call-functions.js';
 export * from './elements/helpers/cell-tasks.js';
