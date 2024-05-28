1c94090ce238bb91c7bb1b7e9e0143a4e02a3655
guillem.cordoba <guillem.cordoba@gmail.com>
2024-05-28 17:05:56 +0200

    Reduced package size

--- packages/cli/client/vite.config.ts
+++ packages/cli/client/vite.config.ts
@@ -1,11 +1,4 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-  ],
-});
-
+export default defineConfig({});
