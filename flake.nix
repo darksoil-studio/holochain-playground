abea3cc086a32b3bb08d0623dade43785806a7cd
guillem.cordoba <guillem.cordoba@gmail.com>
2024-11-06 16:48:36 +0100

    Fixed nixConfig

--- flake.nix
+++ flake.nix
@@ -9,6 +9,17 @@
     tnesh-stack.url = "github:darksoil-studio/tnesh-stack/main-0.3";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://holochain-ci.cachix.org"
+      "https://darksoil-studio.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8="
+      "darksoil-studio.cachix.org-1:UEi+aujy44s41XL/pscLw37KEVpTEIn8N/kn7jO8rkc="
+    ];
+  };
+
   outputs = inputs@{ ... }:
     inputs.holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = builtins.attrNames inputs.holonix.devShells;
